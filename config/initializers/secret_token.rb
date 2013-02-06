b51e8c80f41dde1abd155df11e471b0d8a5bee29
Matt Mo <matt.mo@moovweb.com>
2013-02-05 22:35:46 -0800

    Rails project generation of Language Learning Experiment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Languagelearningexperiment::Application.config.secret_token = 'a35bea57aaf516299830e84548fd72220a65c7456ffb37ec232db1576db98f3baa8adb5b75a4266f07140e0e7b01ef3504592900111658235cb3c5241eca3c1a'
