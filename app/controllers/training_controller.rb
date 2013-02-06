6b7e72107b2d5b0faa2fa096b0a5ffe032af7820
Matt Mo <matt.mo@moovweb.com>
2013-02-06 04:16:31 -0800

    Experimenting with counters and traversing training phase pages.

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -1,13 +1,17 @@
 class TrainingController < ApplicationController
-  # @counter = 0;
-  # @exp_images = ['https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/cat.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/dog.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/horse.jpeg']
-  # @num_trials = 3
+  @@counter = 0;
+  @@exp_images = ['https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/cat.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/dog.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/horse.jpeg']
+  @@num_trials = 3
   
   def start
+    @time = Time.now
+    
   end
   
   def stimulus
-    @training_image = 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/cat.gif'
-    # @training_image = TrainingController.exp_images[counter]
+    # @training_image = 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/cat.gif'
+    @stimulus_counter = @@counter
+    @max_stimuli = @@num_trials
+    @training_image = @@exp_images[@stimulus_counter]
   end
 end
