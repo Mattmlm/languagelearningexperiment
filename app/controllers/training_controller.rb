6aa35b8a12424a46212dcd4674e6a2283ce62268
Matt Mo <matt.mo@moovweb.com>
2013-02-06 10:39:57 -0800

    Added key instead of nyancat for training images.

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -1,6 +1,7 @@
 class TrainingController < ApplicationController
   @@counter = 0;
-  @@exp_images = ['https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/cat.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/dog.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/horse.jpeg']
+  # @@exp_images = ['https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/cat.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/dog.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/horse.jpeg']
+  @@exp_images = ['https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/x1%20key.jpg', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/dog.gif', 'https://dl.dropbox.com/u/883211/Julia%27s%20site/assets/images/horse.jpeg']
   @@num_trials = 3
   
   def start
