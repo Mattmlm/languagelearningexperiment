7ccdd9e465292d5eb2971e990bd7b40ab710140f
Matt Mo <matt.mo@moovweb.com>
2013-02-27 00:12:24 -0800

    Trying to figure out the seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,71 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+UserEntry.delete_all
+IterativeChain.delete_all
+
+# user_entries = UserEntry.create([
+#   {
+#     chain_id: 1,
+#     pairs: [{
+#       "fej": "key",
+#       "feej": "truck",
+#       "jef": "feather",
+#       "jeej": "lamp",
+#       "fef": "window",
+#       "gaab": "rock climbing",
+#       "goob": "swimming",
+#       "boog": "running",
+#       "baab": "jumping on a trampoline",
+#       "goog": "moping"
+#     }]
+#   }
+# ])
+# iterative_chains = IterativeChain.create([
+#     {
+#       num_users: 0
+#     }
+#     chain: {
+#         "user0": {
+#             "fej": "key",
+#             "feej": "truck",
+#             "jef": "feather",
+#             "jeej": "lamp",
+#             "fef": "window",
+#             "gaab": "rock climbing",
+#             "goob": "swimming",
+#             "boog": "running",
+#             "baab": "jumping on a trampoline",
+#             "goog": "moping"
+#         }
+#     }},{
+#     "chain": {
+#         "user0": {
+#             "fej": "key",
+#             "feej": "truck",
+#             "jef": "feather",
+#             "jeej": "lamp",
+#             "fef": "window",
+#             "gaab": "rock climbing",
+#             "goob": "swimming",
+#             "boog": "running",
+#             "baab": "jumping on a trampoline",
+#             "goog": "moping"
+#         }
+#     }},{
+#     "chain": {
+#         "user0": {
+#             "fej": "key",
+#             "feej": "truck",
+#             "jef": "feather",
+#             "jeej": "lamp",
+#             "fef": "window",
+#             "gaab": "rock climbing",
+#             "goob": "swimming",
+#             "boog": "running",
+#             "baab": "jumping on a trampoline",
+#             "goog": "moping"
+#         }
+#     }
+# }])
