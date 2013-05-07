13ee249d9934b5e581c30fbde0927a739b955525
Matt Mo <matt.mo@moovweb.com>
2013-05-07 01:04:53 -0700

    Adding relationship between user_entry and iterative_chain

--- app/models/iterative_chain.rb
+++ app/models/iterative_chain.rb
@@ -1,5 +1,6 @@
 class IterativeChain < ActiveRecord::Base
   attr_accessible :locked
   
+  has_many :user_entries, dependent: :destroy
   
 end
--- app/models/user_entry.rb
+++ app/models/user_entry.rb
@@ -1,4 +1,6 @@
 class UserEntry < ActiveRecord::Base
   attr_accessible :iterative_chain_id, :pairs
   serialize :pairs
+  
+  belongs_to :iterative_chain
 end
