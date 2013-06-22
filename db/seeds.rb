a656c1b81ca4c1959a6e7482b1588a84d4d90b81
Matt Mo <matt.mo@moovweb.com>
2013-06-22 16:12:18 -0700

    Fix seeds.rb. 3 Iterative chains, each with the same first entry, but different last entries.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ IterativeChain.delete_all
 num_chains = 3
 for i in 1..num_chains
   iterative_chain = IterativeChain.create(locked: false)
-  user_entry = iterative_chain.user_entries.create(pairs: [
+  iterative_chain.user_entries.create(pairs: [
     { sound: "fej", image: "key" },
     { sound: "feej", image: "truck" },
     { sound: "jef", image: "feather" },
@@ -25,3 +25,40 @@ for i in 1..num_chains
     { sound: "goog", image: "mopping" }
   ])
 end
+
+IterativeChain.find(1).user_entries.create(pairs: [
+  { sound: "fej", image: "key" },
+  { sound: "feej", image: "truck" },
+  { sound: "jef", image: "feather" },
+  { sound: "jeej", image: "lamp" },
+  { sound: "fef", image: "window" },
+  { sound: "gaab", image: "rock climbing" },
+  { sound: "goob", image: "swimming" },
+  { sound: "boog", image: "running" },
+  { sound: "baab", image: "jumping on trampoline" },
+  { sound: "goog", image: "mopping" }
+])
+IterativeChain.find(2).user_entries.create(pairs: [
+  { sound: "feej", image: "truck" },
+  { sound: "jeej", image: "lamp" },
+  { sound: "fef", image: "window" },
+  { sound: "goob", image: "swimming" },
+  { sound: "boog", image: "running" },
+  { sound: "baab", image: "jumping on trampoline" },
+  { sound: "fej", image: "key" },
+  { sound: "gaab", image: "rock climbing" },
+  { sound: "jef", image: "feather" },
+  { sound: "goog", image: "mopping" }
+])
+IterativeChain.find(3).user_entries.create(pairs: [
+  { sound: "fef", image: "swimming" },
+  { sound: "goob", image: "key" },
+  { sound: "boog", image: "running" },
+  { sound: "feej", image: "truck" },
+  { sound: "jeej", image: "lamp" },
+  { sound: "baab", image: "jumping on trampoline" },
+  { sound: "fej", image: "window" },
+  { sound: "gaab", image: "rock climbing" },
+  { sound: "jef", image: "feather" },
+  { sound: "goog", image: "mopping" }
+])
