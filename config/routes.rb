cccfdd487c1feb328e4ba7f26a5cd9e646671f17
Matt Mo <matt.mo@moovweb.com>
2013-06-30 14:35:32 -0700

    Routes - set up so that '/start' goes to start page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Languagelearningexperiment::Application.routes.draw do
   # match 'iterative_chains/start' => 'iterative_chains#start', :as => 'start'
   # match 'user_entries/training' => 'user_entries#training', :as => 'training'
   
+  match 'start' => 'user_entries#start', :as => 'start'
+  
   resources :iterative_chains do
     get ':controller(/:action(/:id))'
     resources :user_entries
